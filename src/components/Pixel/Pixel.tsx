886c5ddd7d3985f75b333e2be08b1149420104a1
benji77fr <b.girardpro@gmail.com>
2022-04-22 16:04:54 +0200

    Modify props passed to Pixel

--- src/components/Pixel/Pixel.tsx
+++ src/components/Pixel/Pixel.tsx
@@ -1,4 +1,9 @@
-import React, { useState } from 'react'
+import { on } from 'events';
+import { useState } from 'react'
+
+import { useStore } from '../../store/color.store'
+
+import './pixel.scss'
 
 interface IPixel {
     x: number;
@@ -7,14 +12,67 @@ interface IPixel {
 }
 
 function Pixel({ x, y, color }: IPixel): JSX.Element {
-  const [ currentColor, setCurrentColor ] = useState()
-  const [ oldColor, setOldColor ] = useState()
-  const [ opacity, setOpacity ] = useState()
+  const [ currentColor, setCurrentColor ] = useState<string>()
+  const [ opacity, setOpacity ] = useState<number>()
+  const paintColor = useStore((state) => state.paintColor)
 
-  
+  const setColor = () => {
+    setCurrentColor(paintColor)
+    setOpacity(1)
+  }
+
+  const eraseColor = () => {
+    setOpacity(0)
+  }
+
+  const onClick = (event: any) => {
+    setColor()
+    event.preventDefault()
+  }
+
+  const onContextMenu = (event: any) => {
+    eraseColor()
+    event.preventDefault()
+  }
+
+  const onMouseOver = (event: any) => {
+    if (event.buttons === 0 && opacity === 0) {
+      setOpacity(0.18)
+    } else if (event.buttons === 1) {
+      setColor()
+    } else if (event.buttons === 2) {
+      eraseColor()
+    }
+  }
+
+  const onMouseLeave = () => {
+    if(opacity === 0.18) {
+      eraseColor()
+    }
+  }
 
   return (
-    <div></div>
+    <>
+      <div
+        className='pixel'
+        style={{
+          boxShadow: `${x}rem ${y}rem 0 -0.05rem ${color}`
+        }}
+      />
+      <div 
+        className='pixel hover-pixel'
+        style={{
+          top: `${y}rem`,
+          left: `${x}rem`,
+          backgroundColor: currentColor,
+          opacity: opacity
+        }}
+        onMouseOver={onMouseOver}
+        onMouseLeave={onMouseLeave}
+        onClick={onClick}
+        onContextMenu={onContextMenu}
+      />
+    </>
   )
 }
 
